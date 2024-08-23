8cccaed944fb9ac31510007ecd82fbf252b8d02b
emusenule <emusenjuleww@gmail.com>
2024-08-23 13:53:55 -0700

    sorting algorithms

--- print_array.c
+++ print_array.c
@@ -1,23 +1,15 @@
-#include <stdlib.h>
-#include <stdio.h>
+#include "sort.h"
 
-/**
- * print_array - Prints an array of integers
- *
- * @array: The array to be printed
- * @size: Number of elements in @array
- */
 void print_array(const int *array, size_t size)
 {
-    size_t i;
+    if (array == NULL)
+        return;
 
-    i = 0;
-    while (array && i < size)
+    for (size_t i = 0; i < size; i++)
     {
-        if (i > 0)
-            printf(", ");
         printf("%d", array[i]);
-        ++i;
+        if (i < size - 1)
+            printf(", ");
     }
     printf("\n");
 }
--- sort.h
+++ sort.h
@@ -2,24 +2,8 @@
 #define SORT_H
 
 #include <stdio.h>
-#include <stdlib.h>
 
-/**
- * struct listint_s - Doubly linked list node
- *
- * @n: Integer stored in the node
- * @prev: Pointer to the previous element of the list
- * @next: Pointer to the next element of the list
- */
-typedef struct listint_s
-{
-    int n;
-    struct listint_s *prev;
-    struct listint_s *next;
-} listint_t;
-
-/* Function prototypes */
-void print_list(const listint_t *list);
-void insertion_sort_list(listint_t **list);
+void print_array(const int *array, size_t size);
+void selection_sort(int *array, size_t size);
 
 #endif /* SORT_H */
